40ccf344fc049d7638b7cee47179b8fd7b399092
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-10-25 10:39:33 -0300

    Configure test folder structure

---
+++ portal/tests/test_views.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+from django.test import SimpleTestCase
+from django.urls import resolve, reverse
+
+from portal.views import DashboardView
+
+
+class DashboardPageTests(SimpleTestCase):
+    """
+    Test suite for the portal main dashboard page.
+    """
+
+    pass
