b8b63bdae2390104e1ecceb0d1e956917df5b2cc
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-10-29 22:55:26 -0300

    Initial portal unit tests 🔥

--- portal/tests/test_views.py
+++ portal/tests/test_views.py
@@ -1,14 +1,23 @@
-from http import HTTPStatus
-
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import resolve, reverse
 
 from portal.views import DashboardView
+from properties.factories import PropertyFactory
+from users.factories import PropertyOwnerFactory
 
 
-class DashboardPageTests(SimpleTestCase):
+class DashboardPageTests(TestCase):
     """
     Test suite for the portal main dashboard page.
     """
 
-    pass
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.template_name = "portal/dashboard.html"
+        cls.owner = PropertyOwnerFactory()
+        cls.property = PropertyFactory(owner=cls.owner)
+        cls.url = reverse("portal:dashboard", kwargs={"slug": cls.property.slug})
+
+    def test_dashboard_url_resolves_correct_view(self):
+        view = resolve(self.url)
+        self.assertEqual(view.func.__name__, DashboardView.as_view().__name__)
